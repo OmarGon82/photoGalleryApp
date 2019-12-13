3e359f5aedeee3edd43acd67f277f7e51adc9b90
Omar Gonzalez <gonzalezoe82@gmail.com>
2019-12-13 16:24:08 +0900

    added notFound svg

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,65 +1,28 @@
 import React from 'react';
-import $ from "jquery";
+// import icon from './images/icon.svg'; 
 
 
 
 const NotFound = () => (
-  
-  var formatThousandsNoRounding = function(n, dp){
-    var e = '', s = e+n, l = s.length, b = n < 0 ? 1 : 0,
-        i = s.lastIndexOf(','), j = i == -1 ? l : i,
-        r = e, d = s.substr(j+1, dp);
-    while ( (j-=3) > b ) { r = '.' + s.substr(j, 3) + r; }
-    return s.substr(0, j + 3) + r + 
-      (dp ? ',' + d + ( d.length < dp ? 
-          ('00000').substr(0, dp - d.length):e):e);
-  };
-  
-  var hasRun = false;
-  
-  inView('#countUp').on('enter', function() {
-      if (hasRun == false) {
-          $('.number').each(function() {
-              var $this = $(this),
-                  countTo = $this.attr('data-count');
-  
-              $({ countNum: $this.text()}).animate({
-                  countNum: countTo
-              },
-              {
-                  duration: 2000,
-                  easing:'linear',
-                  step: function() {
-                      $this.text(formatThousandsNoRounding(Math.floor(this.countNum)));
-                  },
-                  complete: function() {
-                      $this.text(formatThousandsNoRounding(this.countNum));
-                  }
-              });
-          });
-          hasRun = true;
-      }
-  });
 
-    Render (
-        <div class="container">
-      <div class="row">
-          <div class="xs-12 md-6 mx-auto">
-              <div id="countUp">
-                  <div class="number" data-count="404">0</div>
-                  <div class="text">Page not found</div>
-                  <div class="text">This may not mean anything.</div>
-                  <div class="text">I'm probably working on something that has blown up.</div>
-              </div>
-          </div>
-      </div>
-  </div>      
-  )
+    <div className="main-content not-found">
+      <i className="material-icons icn-error">error_outline</i>
+      <h2>Page Not Found</h2>
+      <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <defs>
+      <style>.cls-1 fill: #141f38;</style>
+      </defs>
+      <title>document-denied-outline</title>
+      <path class="cls-1" d="M128,268.8h76.8a12.8,12.8,0,0,0,0-25.6H128a12.8,12.8,0,0,0,0,25.6Z"/>
+      <path class="cls-1" d="M281.6,294.4H128a12.8,12.8,0,0,0,0,25.6H281.6a12.8,12.8,0,0,0,0-25.6Z"/>
+      <path class="cls-1" d="M281.22,345.6H128a12.8,12.8,0,0,0,0,25.6H281.22a12.8,12.8,0,0,0,0-25.6Z"/>
+      <path class="cls-1" d="M422.4,333.82V153.6L268.94,0H76.8a64,64,0,0,0-64,64V448a64,64,0,0,0,64,64H358.4a63.4,63.4,0,0,0,23.33-4.47A89.54,89.54,0,1,0,422.4,333.82ZM356.24,457.66A63.94,63.94,0,0,1,444.86,369ZM373.09,140.8H320a38.44,38.44,0,0,1-38.4-38.4V49.08ZM76.8,486.4A38.44,38.44,0,0,1,38.4,448V64A38.44,38.44,0,0,1,76.8,25.6H256v76.8a64,64,0,0,0,64,64h76.8V333.82A89.48,89.48,0,0,0,347,486.4Zm332.8,0a63.6,63.6,0,0,1-35.26-10.64L463,387.14A63.94,63.94,0,0,1,409.6,486.4Z"/>
+      </svg>
+      <a href='https://dryicons.com/free-icons/404-error'> Icon by Dryicons </a>
+    </div>  
+  
 );
 
 export default NotFound;
 
-{/* <div className="main-content not-found">
-<i className="material-icons icn-error">error_outline</i>
-<h2>Page Not Found</h2>
-</div> */}
+// viewBox="0 0 512 512"
