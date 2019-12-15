ce76ed4eeaf9d7969a38c1abe6e95eb79cbf5d62
Omar Gonzalez <gonzalezoe82@gmail.com>
2019-12-15 10:13:10 +0900

    Updates

---
+++ precache-manifest.bdad513716b24ceb972070ee07019f69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b8cee08a45c13405330970ab7d5c05f",
+    "url": "/photoGalleryApp/index.html"
+  },
+  {
+    "revision": "734ad1786d11a33e1b56",
+    "url": "/photoGalleryApp/static/css/main.5924dbfc.chunk.css"
+  },
+  {
+    "revision": "b3d78030475235f639d2",
+    "url": "/photoGalleryApp/static/js/2.f13ae194.chunk.js"
+  },
+  {
+    "revision": "734ad1786d11a33e1b56",
+    "url": "/photoGalleryApp/static/js/main.675c4ede.chunk.js"
+  },
+  {
+    "revision": "c7478d360c0cf1b93495",
+    "url": "/photoGalleryApp/static/js/runtime-main.ad27eca3.js"
+  }
+]);
